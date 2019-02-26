bb18858823f08540c00e3b6bf081a4f9d1802a59
g-strzelecki <grzegorz.strzelecki84@gmail.com>
2019-02-26 17:57:20 +0100

    Changes added after 1st review

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$('button').on('click', function(){
-    $('.circle').toggleClass('active');
+document.addEventListener('DOMContentLoaded', function() {
+
+let content = document.getElementById("circle");
+let button = document.getElementById("button");
+
+button.onclick = function() {
+
+    if (content.className == "circle") {
+        content.className = "circle active";
+    } else {
+        content.className = "circle";
+    }
+
+};
+
 })
